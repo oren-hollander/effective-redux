1a6ad3aa71ffc00e63dc3d2ad504914e28d00a73
orenh <orenh@wix.com>
2017-11-29 22:20:08 +0200

    remove unrelated test

--- src/lo.test.js
+++
@@ -1,6 +0,0 @@
-import { remove, eq, get, compose } from 'lodash/fp'
-
-test('', () => {
-  const x = [{id: 1}, {id: 2}, {id: 3}]
-  expect(remove(compose(eq(2), get('id')), x)).toEqual([{id: 1}, {id: 3}])
-})
