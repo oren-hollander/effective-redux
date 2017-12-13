56a6d70914a88f34d9a892503cca3ad497e855dd
Oren Hollander <orenh@wix.com>
2017-12-13 19:17:18 +0200

    only notify affected fragment stores

--- src/util/fragmentStore.js
+++ src/util/fragmentStore.js
@@ -1,49 +1,48 @@
 import { forEach, concat, without, isUndefined, mapValues, toPairs, fromPairs, map, flow, spread, get, deffaultTo } from 'lodash/fp'
 import { invoke } from './invoke'
 
-export const fragmentStore = (key, store) => {
+export const fragmentStore = (fragmentId, store) => {
   let subscribers = []
 
   const subscribe = subscriber => {
-    subscribers = concat(subscribers, subscriber)
+    subscribers = concat(subscribers, [subscriber])
     return () => {
       subscribers = without(subscribers, [subscriber])
     }
   }
 
-  const notifySubscribers = () => forEach(invoke(), subscribers)
+  const getState = () => store.getState()[fragmentId]
   
-  const dispatch = action => {
-    store.dispatch(action)
-    notifySubscribers()
-  }
-
-  const getState = () => store.getState()[key]
+  store.subscribe(() => {
+    const currentState = store.getState()[fragmentId]
+      if(store.getState().lastActionFragmentId === fragmentId){
+        forEach(invoke(), subscribers)
+      }
+  })
 
   return {
     ...store,
     subscribe,
-    dispatch,
     getState
   }
 }
 
 export const combineFragmentReducers = reducers => 
   (state, action) => {
-    if(action.type === '@@redux/INIT'){
-      return flow(
-        toPairs,
-        map(([fragmentId, fragmentReducer]) => [fragmentId, fragmentReducer(get(action.fragmentId, state), action)]),
-        fromPairs
-      )(reducers) 
-    } 
-    else {
-      return { 
+    const newState = action.type === '@@redux/INIT'
+      ? flow(
+          toPairs,
+          map(([fragmentId, fragmentReducer]) => [fragmentId, fragmentReducer(get(action.fragmentId, state), action)]),
+          fromPairs
+        )(reducers) 
+      : { 
         ...state, 
         [action.fragmentId]: reducers[action.fragmentId](state[action.fragmentId], action) 
       }
-    }
+
+      return { ...newState, lastActionFragmentId: action.fragmentId }
   }
+
 
 export const replaceFragmentReducers = (reducers, store) => {
   store.replaceReducer(combineFragmentReducers(reducers))
--- src/util/fragmentStore.test.js
+++ src/util/fragmentStore.test.js
@@ -5,14 +5,23 @@ describe('fragment store', () => {
   const fragment1Reducer = (state = {name: 'fragment 1'}) => state
   const fragment2Reducer = (state = {name: 'fragment 2'}) => state
 
+  const action1 = {type: 'action', fragmentId: 'fragment1'}
+  const action2 = {type: 'action', fragmentId: 'fragment2'}
+
   const reducer = combineFragmentReducers({ 
     fragment1: fragment1Reducer,
     fragment2: fragment2Reducer     
   })
   
-  const store = createStore(reducer)
-  const fragment1Store = fragmentStore('fragment1', store)
-  const fragment2Store = fragmentStore('fragment2', store)
+  let store
+  let fragment1Store
+  let fragment2Store
+
+  beforeEach(() => {
+    store = createStore(reducer)
+    fragment1Store = fragmentStore('fragment1', store)
+    fragment2Store = fragmentStore('fragment2', store)  
+  })
 
   test('getState should return only fragment state', () => {
     expect(fragment1Store.getState()).toEqual({name: 'fragment 1'})
@@ -26,13 +35,24 @@ describe('fragment store', () => {
     fragment1Store.subscribe(fragment1Callback)
     fragment2Store.subscribe(fragment2Callback)
 
-    fragment1Store.dispatch({type: 'action', fragmentId: 'fragment1'})
-    fragment2Store.dispatch({type: 'action', fragmentId: 'fragment2'})
+    fragment1Store.dispatch(action1)
+    fragment2Store.dispatch(action2)
 
     expect(fragment1Callback).toHaveBeenCalledTimes(1)
     expect(fragment2Callback).toHaveBeenCalledTimes(1)
   })
 
+  test('notify the correct subscriber when dispatching an action via another fragment store', () => {
+    const fragment1Callback = jest.fn()
+    const fragment2Callback = jest.fn()
+    
+    fragment1Store.subscribe(fragment1Callback)
+    fragment2Store.subscribe(fragment2Callback)
+    fragment2Store.dispatch(action1)
+    expect(fragment1Callback).toHaveBeenCalledTimes(1)
+    expect(fragment2Callback).not.toHaveBeenCalled()
+  })
+  
   test('replace fragment reducers', () => {
     const fragment3Reducer = (state = {name: 'fragment 3'}) => state
     const fragment3Store = fragmentStore('fragment3', store)
